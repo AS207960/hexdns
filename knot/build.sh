b95e8ea73aa3de6d10b34777fbe785d3fc25c995
Q <q@misell.cymru>
2022-06-21 23:07:21 +0100

    add logging to sidecar reloader

---
+++ knot/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+VERSION=$(sentry-cli releases propose-version || exit)
+
+docker build -t "as207960/hexdns-knot-sidecar:$VERSION" . || exit
+docker push "as207960/hexdns-knot-sidecar:$VERSION"
---
+++ knot/sidecar.py
@@ -0,0 +1,43 @@
+import pika
+import libknot.control
+import os
+
+libknot.Knot(os.getenv("LIBKNOT_PATH", "/usr/lib/x86_64-linux-gnu/libknot.so.11"))
+
+
+def main():
+    parameters = pika.URLParameters(os.getenv("RABBITMQ_RPC_URL"))
+    connection = pika.BlockingConnection(parameters=parameters)
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='hexdns_primary_reload', exchange_type='fanout', durable=True)
+
+    queue = channel.queue_declare(queue='', exclusive=True)
+    channel.queue_bind(exchange='hexdns_primary_reload', queue=queue.method.queue)
+
+    channel.basic_qos(prefetch_count=0)
+    channel.basic_consume(queue=queue.method.queue, on_message_callback=callback, auto_ack=False)
+
+    print("RPC handler now running", flush=True)
+    try:
+        channel.start_consuming()
+    except (KeyboardInterrupt, SystemExit):
+        print("Exiting...", flush=True)
+
+
+def callback(channel, method, properties, body: bytes):
+    zone = body.decode()
+    print(f"Reloading {zone}", flush=True)
+    ctl = libknot.control.KnotCtl()
+    ctl.connect("/rundir/knot.sock")
+
+    try:
+        ctl.send_block(cmd="zone-reload", zone=zone)
+        print(f"Reloaded {zone}", flush=True)
+    finally:
+        ctl.send(libknot.control.KnotCtlType.END)
+        ctl.close()
+
+
+if __name__ == "__main__":
+    main()
