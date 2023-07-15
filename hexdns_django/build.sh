ccf2d52c496c4e09ed8f85c2eb5f4543732075bb
Q <q@misell.cymru>
2023-07-15 20:52:05 +0100

    fix build

--- hexdns_django/build.sh
+++ hexdns_django/build.sh
@@ -4,6 +4,3 @@ VERSION=$(sentry-cli releases propose-version || exit)
 
 docker buildx build --platform linux/amd64 --push -t "as207960/hexdns-django:$VERSION" . || exit
 docker buildx build --platform linux/amd64 --push -t "as207960/hexdns-django-root:$VERSION" -f Dockerfile.root . || exit
-
-sentry-cli releases --org as207960 new -p hexdns "$VERSION" || exit
-sentry-cli releases --org as207960 set-commits --auto "$VERSION"
