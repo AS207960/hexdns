039ec79f8b611c6c74fc9bc5e86f10bce4656485
Q <q@misell.cymru>
2023-11-14 23:25:33 +0000

    dnssec signer deployment ksk

--- hexdns_django/kube/apply.sh
+++ hexdns_django/kube/apply.sh
@@ -5,6 +5,7 @@ VERSION=$(sentry-cli releases propose-version || exit)
 sed -e "s/(version)/$VERSION/g" < django.yaml | kubectl apply -f - || exit
 sed -e "s/(version)/$VERSION/g" < knot.yaml | kubectl apply -f - || exit
 sed -e "s/(version)/$VERSION/g" < axfr.yaml | kubectl apply -f - || exit
+sed -e "s/(version)/$VERSION/g" < dnssec.yaml | kubectl apply -f - || exit
 kubectl apply -f kres.yaml || exit
 
 sentry-cli releases --org as207960 deploys $VERSION new -e prod
