bab1efae0900edf8d13561b1d1e827d61c1b5b3b
Q <q@misell.cymru>
2024-01-04 14:20:33 +0100

    fix resign of zones

--- hexdns_django/dns_grpc/management/commands/update-all-zones.py
+++ hexdns_django/dns_grpc/management/commands/update-all-zones.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.db.models import Q
 from dns_grpc import models, tasks
 
 
@@ -11,11 +12,13 @@ class Command(BaseCommand):
         last_resign_cutoff = now - timezone.timedelta(days=1)
 
         for zone in models.DNSZone.objects.filter(
-            last_resign__lt=last_resign_cutoff
+            Q(last_resign__lt=last_resign_cutoff) | Q(last_resign__isnull=True)
         ):
+            print(f"Updating forward zone {zone.zone_root} - {zone.id}")
             tasks.update_fzone.delay(zone.id)
 
         for zone in models.ReverseDNSZone.objects.filter(
-            last_resign__lt=last_resign_cutoff
+            Q(last_resign__lt=last_resign_cutoff) | Q(last_resign__isnull=True)
         ):
+            print(f"Updating reverse zone {zone.zone_root_address}/{zone.zone_root_prefix} - {zone.id}")
             tasks.update_rzone.delay(zone.id)
