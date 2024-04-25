0144c12f846c17e8f6dccc2a0a804528daa134fb
Q <q@misell.cymru>
2024-04-25 16:21:50 +0100

    celery state

--- hexdns_django/hexdns_django/celery.py
+++ hexdns_django/hexdns_django/celery.py
@@ -8,6 +8,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.update(
     task_acks_late=True,
     task_reject_on_worker_lost=True,
-    worker_state_db="./celery-state.db",
+    worker_state_db="/celery-state/celery-state.db",
 )
 app.autodiscover_tasks()
