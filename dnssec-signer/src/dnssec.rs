dec61650fe8b730b463ab4e17fa18b916b4057a9
Q Misell <q@magicalcodewit.ch>
2025-05-15 17:57:13 +0200

    fix ed25519 sig

--- dnssec-signer/src/dnssec.rs
+++ dnssec-signer/src/dnssec.rs
@@ -298,7 +298,7 @@ pub fn sign_zone(
                 openssl::pkey::Id::ED25519 => {
                     let mut signer = openssl::sign::Signer::new_without_digest(&key).unwrap();
                     let mut hasher = openssl::hash::Hasher::new(
-                        openssl::hash::MessageDigest::sha256()
+                        openssl::hash::MessageDigest::sha512()
                     ).unwrap();
                     hasher.update(&tbs).unwrap();
                     let hash = hasher.finish().unwrap();
