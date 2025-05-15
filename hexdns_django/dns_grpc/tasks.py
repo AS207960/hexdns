0600c2488593092be0b1af9a26bcf07377584b27
Q Misell <q@magicalcodewit.ch>
2025-05-15 16:56:49 +0200

    fix signal zone key

--- hexdns_django/dns_grpc/tasks.py
+++ hexdns_django/dns_grpc/tasks.py
@@ -397,7 +397,7 @@ def write_zone_file(zone_contents: str, priv_key: typing.List[str], zone_name: s
     )
     if priv_key:
         zone_storage.save(
-            f"{zone_name}key", django.core.files.base.ContentFile("\n\n".join(priv_key).encode())
+            f"{zone_name}key", django.core.files.base.ContentFile("\n\n".join(priv_key).strip().encode())
         )
 
 
